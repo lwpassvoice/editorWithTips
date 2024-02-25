357a998712591f2ee947609885a9c2c5c97d1adc
lwpassvoice <zgdxst@qq.com>
2024-02-25 21:51:23 +0800

    feat(app): using editor in app

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,18 @@ import EditorWithTips from './Editor/EditorWithTips'
 function App() {
 
   return (
-    <>
-      <div>editor-with-tips</div>
+    <div className='app-warpper'>
+      <h3>Textarea:</h3>
       <EditorWithTips>
         <textarea></textarea>
       </EditorWithTips>
+      <br />
+      <h3>ContentEditable:</h3>
       <EditorWithTips>
         <div contentEditable></div>
       </EditorWithTips>
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
